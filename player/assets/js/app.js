53e65269d8c8c340bb14c8301a574015181a5ec9
chadhartman <chad+github@tealium.com>
2017-06-29 11:18:06 -0700

    Now playing cards

--- player/assets/js/app.js
+++ player/assets/js/app.js
@@ -2,29 +2,77 @@
 
 var app = angular.module('playerApp', []);
 
+app.util = {
+    normalizeText: function (text) {
+
+        text = text
+            .toLowerCase()
+            .replace(/[^a-z0-9]|\s+|\r?\n|\r/gmi, ' ')
+            .trim()
+            .replace(/ /g, '_');
+
+        let lastLen;
+        do {
+            lastLen = text.length;
+            text = text.replace('__', '_');
+        } while (lastLen !== text.length);
+
+        return text;
+    },
+    popRandom: function (array) {
+        let index = Math.floor(Math.random() * array.length);
+        return array.splice(index, 1)[0];
+    }
+};
+
 app.controller('PlayerController', ['$scope', function ($scope) {
 
-    $scope.config = {};
+    // TODO: remove constants
+    $scope.config = {
+        username: "Chad",
+        opponent: "Seth",
+        deck: "Celebration\nCovering Fire\nDeflection\nElement of Surprise\nFleet Footed\nMarksman\nPlanning\nRecovery\nTake Cover\nTake Initiative\nTelekinetic Throw\nUrgency\nForce Lightning\nLord of the Sith\nLure of the Dark Side",
+        begin: function () {
 
-    $scope.playClick = function () {
+            if (!$scope.config.username) {
+                $scope.config.error = "Missing username";
+                return;
+            }
 
-        if (!$scope.config.username) {
-            $scope.config.error = "Missing username";
-            console.log(config);
-            return;
+            if (!$scope.config.opponent) {
+                $scope.config.error = "Missing opponent's username";
+                return;
+            }
+
+            if (!$scope.config.deck) {
+                $scope.config.error = "Missing deck";
+                return;
+            }
+
+            $scope.config.play = true;
+
+            $scope.config.deck.split("\n").forEach(function (element) {
+                let id = app.util.normalizeText(element);
+                $scope.player.deck.push({
+                    name: element,
+                    id: id
+                });
+            });
+
+            app.scope = $scope;
         }
+    }
 
-        if (!$scope.config.opponent) {
-            $scope.config.error = "Missing opponent's username";
-            return;
+    $scope.player = {
+        deck: [],
+        hand: [],
+        played: [],
+        discarded: [],
+        moveRandom: function (origin, destination) {
+            destination.push(app.util.popRandom(origin));
+        },
+        move: function (index, origin, destination) {
+            destination.push(origin.splice(index, 1)[0]);
         }
-
-        if (!$scope.config.deck) {
-            $scope.config.error = "Missing deck";
-            return;
-        }
-
-        $scope.config.play = true;
     };
-
 }]);
